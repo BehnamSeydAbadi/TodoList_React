81ca362129ae054d320120de82418da08aee822d
Behnam S.A <behnamsa96@outlook.com>
2021-09-18 21:19:41 +0430

    header done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
-// import './App.css';
-import './styles/styles.css'
+import "./styles/styles.css";
 
 function App() {
   return (
     <main>
-      <div className="card">
-        hello world
+      <div className='card'>
+        <div className='card-header'>
+          Todo List
+          <div className='card-description'>
+            Get things done, one item at a time.
+          </div>
+          <div className='card-line'></div>
+        </div>
       </div>
     </main>
   );
